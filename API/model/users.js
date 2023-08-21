83c00a20344bb33b402bb493aa43f930595f6660
Samkelo <sammpokela@gmail.com>
2023-08-21 14:27:24 +0200

    new

--- API/model/users.js
+++ API/model/users.js
@@ -1,137 +1,127 @@
-const db = require("../config") //this imprt the db con from config
-const {hash, compare, hashSync} = require('bcrypt')
-const {createToken} = require('../middleware/authentication')
-class Users{
-    fetchUsers(req, res){
+const db = require("../config"); //this imprt the db con from config
+const { hash, compare, hashSync } = require("bcrypt");
+const { createToken } = require("../middleware/authentication");
+class Users {
+    fetchUsers(req, res) {
         const query = `
-        SELECT userID, firstName, lastName, userAge, Gender, userRole,
-        emailAdd, userProfile
-        FROM Users;
-        `
-        db.query(query,(err, results)=>{
-            if(err)throw err
-            res.json({
-                status: res.stausCode,
-                results
-            })
-        })
-    }
-    fetchUser(req, res){
-        const query = `
-        SELECT userID, firstName, lastName, userAge, Gender, userRole,
-        emailAdd,userProfile
+            SELECT userID, firstName, lastName, userAge, gender, userRole,
+            emailAdd, userPass, profileUrl
+            FROM Users;
+        `;
+        db.query(query, (err, results) => {
+          if (err) throw err;
+          res.json({
+            status: res.statusCode, // Corrected typo here
+            results,
+          });
+        });
+      }
+  fetchUser(req, res) {
+    const query = `
+        SELECT userID, firstName, lastNmae,userAge, gender,userRole,
+        emailAdd, profileUrl
         FROM Users
         WHERE userId = ${req.params.id};
-        `
-        db.query(query,
-            (err, result)=>{
-                if (err) throw err
-                res.json({
-                    status: res.statusCode,
-                    result
-                })
-            })
-    }
-     async register(req, res){
-      const data = req.body
-      //encrypt password
-      data.userPass = await hash(data.userPass, 15)
-      //PAYLOAD means DATA THAT COMES FROM THE USER
-      const user = {
-        emailAdd: data.emailAdd,
-        userPass: data.userPass
-      }
-      //query
-      const query = `
+        `;
+    db.query(query, (err, result) => {
+      if (err) throw err;
+      res.json({
+        status: res.statusCode,
+        result,
+      });
+    });
+  }
+  async register(req, res) {
+    const data = req.body;
+    //encrypt password
+    data.userPass = await hash(data.userPass, 15);
+    //PAYLOAD means DATA THAT COMES FROM THE USER
+    const user = {
+      emailAdd: data.emailAdd,
+      userPass: data.userPass,
+    };
+    //query
+    const query = `
       INSERT INTO Users
       SET ?;
-      `
-      db.query(query,[data], (err)=>{
-        if(err) throw err
-        //create a token
-        let token = createToken(user)
-            res.cookie("LegitUser", token, {
-                maxAge:  3600000,
-                httpOnly: true
-            });
-            res.json({
-                status: res.statusCode,
-                msg: "You are now registered."
-            })
-      })
-    }
-    login(req, res) {
-        const { emailAdd, userPass } = req.body;
-        // Use parameterized query
-        const query = `
-        SELECT userID, firstName, lastName, userAge, Gender, userRole,
-        emailAdd,userProfile
-        FROM Users
-        WHERE emailAdd = '${emailAdd}';
-        `;
-        db.query(query, async (err, result) => {
-            if (err) throw err;
-            if (!result?.length) {
-                res.json({
-                    status: res.statusCode,
-                    msg: "You provided a wrong email."
-                });
-            } else {
-                await compare(userPass, result[0].userPass, (cErr, cResult) => {
-                    if (cErr) throw cErr;
-                    // Create a token
-                    const token = createToken({
-                        emailAdd,
-                        userPass
-                    })
-                    if (cResult) {
-                        res.json({
-                            msg: "Logged in",
-                            token,
-                            result: result[0]
-                        });
-                    } else {
-                        res.json({
-                            status: res.statusCode,
-                            msg: "Invalid password or you have not registered"
-                        });
-                    }
-                });
-            }
+      `;
+    db.query(query, [data], (err) => {
+      if (err) throw err;
+      //create a token
+      let token = createToken(user);
+      res.json({
+        status: res.statusCode,
+        msg: "You are now registered.",
+      });
+    });
+  }
+  login(req, res) {
+    const { emailAdd, userPass } = req.body;
+    const query = `
+      SELECT userID, firstName, lastName, userAge, gender, userRole,
+      emailAdd, profileUrl, userPass
+      FROM Users
+      WHERE emailAdd = ?;
+    `;
+    db.query(query, [emailAdd], async (err, result) => {
+      if (err) throw err;
+      if (!result?.length) {
+        res.json({
+          status: res.statusCode,
+          msg: "You provided a wrong email.",
         });
+      } else {
+        await compare(userPass, result[0].userPass, (cErr, cResult) => {
+          if (cErr) throw cErr;
+          // Create a token
+          const token = createToken({
+            emailAdd,
+            userPass,
+          });
+          if (cResult) {
+            res.json({
+              msg: "Logged in",
+              token,
+              result: result[0],
+            });
+          } else {
+            res.json({
+              status: res.statusCode,
+              msg: "Invalid password or you have not registered",
+            });
+          }
+        });
+      }
+    });
+  }
+  updateUser(req, res) {
+    const data = req.body;
+    if (data.userPass) {
+      data.userPass = hashSync(data.userPass, 15);
     }
-    updateUser(req, res){
-        const data = req.body
-        if (data.userPass){
-            data.userPass =
-            hashSync(data.userPass, 15)
-        }
-        const query =`
-        UPDATE User
+    const query = `
+        UPDATE Users
         SET ?
         WHERE userID = ?
-        `
-        db.query(query,
-            [req.body, req.params.id],
-            (err)=>{
-                if(err) throw err
-                res.json({
-                    status: res.statusCode,
-                    msg: "The user record was updated."
-                })
-            })
-    }
-    deleteUser(req, res){
-        const query = ` DELETE FROM Users
-        WHERE userID = ${req.params.id};
-        `
-        db.query(query,(err)=>{
-            if(err) throw err
-            res.json({
-                status: res.statusCode,
-                msg: "A user recored was deleted."
-            })
-        })
-    }
+        `;
+    db.query(query, [data, req.params.id], (err) => {
+      if (err) throw err;
+      res.json({
+        status: res.statusCode,
+        msg: "The user record was updated.",
+      });
+    });
+  }
+  deleteUser(req, res) {
+    const query = `DELETE FROM Users
+        WHERE userID = ?`;
+    db.query(query, [req.params.id], (err) => {
+      if (err) throw err;
+      res.json({
+        status: res.statusCode,
+        msg: "A user record was deleted.",
+      });
+    });
+  }
 }
-module.exports = Users
+module.exports = Users;
