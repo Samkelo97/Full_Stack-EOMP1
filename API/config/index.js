8ae41972339766ab0c760072a379f30dcb4fbba2
Samkelo <sammpokela@gmail.com>
2023-08-22 11:07:47 +0200

    update

--- API/config/index.js
+++ API/config/index.js
@@ -5,6 +5,7 @@ const connection = createPool({
     database: process.env.dbName,
     user: process.env.dbUser,
     password:process.env.dbPwd,
+    port: process.env.dbPort,
     multipleStatements: true,
     connectionLimit:30,
 });
