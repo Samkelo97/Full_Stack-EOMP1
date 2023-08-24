30014a5de99288132e332cb1d5c6038aea7cad23
Samkelo <sammpokela@gmail.com>
2023-08-24 11:52:49 +0200

    update

--- API/index.js
+++ API/index.js
@@ -15,6 +15,7 @@ app.use((req, res, next) => {
     res.header("Access-Control-Request-Methods", "*");
     res.header("Access-Control-Allow-Headers", "*");
     res.header("Access-Control-Expose-Headers", "Authorization");
+    res.header("Access-Control-Expose-Headers", "http://localhost:8080");
     next();
   });
   app.use(
