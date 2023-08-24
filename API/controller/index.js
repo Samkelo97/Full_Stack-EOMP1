5cc765b4ff5a7da876f4722149ab28d95aea0a2d
Samkelo <sammpokela@gmail.com>
2023-08-24 11:41:34 +0200

    update

--- API/controller/index.js
+++ API/controller/index.js
@@ -43,21 +43,7 @@ routes.get('/products/:id', (req, res) => {
 routes.post('/add-products', bodyParser.json(), (req, res) => {
     products.addProduct(req, res)
 })
-// Update a single route route
-routes.put('/products/:id', bodyParser.json(), (req, res) => {
-    products.updateProduct(req, res)
-})
-routes.get('/products', (req, res) => {
-    products.getProducts(req, res)
-})
-// Get a single product route
-routes.get('/products/:id', (req, res) => {
-    products.getProduct(req, res)
-})
-// Add a product route
-routes.post('/add-product', bodyParser.json(), (req, res) => {
-    products.addProduct(req, res)
-})
+
 // Update a single route route
 routes.put('/products/:id', bodyParser.json(), (req, res) => {
     products.updateProduct(req, res)
@@ -70,9 +56,7 @@ routes.delete('/products/:id', (req, res) => {
     products.deleteProduct(req, res)
 })
 // Delete a product route
-routes.delete('/products/:id', (req, res) => {
-    products.deleteProduct(req, res)
-})
+
 module.exports = {
     express,
     routes,
